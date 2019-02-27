9ecfd9cbf333da980a4becd95cea559f5cceb37a
Tanmay Bhattacharya <tanmay.bhattacharya.smit@gmail.com>
2019-02-28 01:15:48 +0530

    writermonad

--- src/Ml.hs
+++ src/Ml.hs
@@ -1,9 +1,13 @@
 module Ml where
 
+import Control.Monad.Writer
+
 data ShoppingList a
   = Empty
   | Item a
  -- | Listofitems [ShoppingList a]
+
+
   deriving (Show)
 
 instance Functor ShoppingList where
@@ -11,12 +15,23 @@ instance Functor ShoppingList where
   fmap f (Item i) = Item (f i)
   --fmap f (Listofitems ls) = Listofitems $ fmap (fmap f) ls
 
+
+
 instance Applicative ShoppingList where
   pure x = Empty
   f <*> Empty = Empty
   Item f <*> Item a = Item $ f a
   --Item f <*> Listofitems a = Listofitems $ (f <*>) <*> a
+
+
   --fmap f (Listofitems ((Item a):Listofitems [as])) = Listofitems $ (fmap f (Item a)) (fmap f Listofitems [as])
+
+
+
+instance Monad ShoppingList where
+  return x = Empty
+  Empty >>= f = Empty
+  Item a >>= f = f a
 
 createList :: [ShoppingList String]
 createList = [Item "Apple", Item "Banana"]
@@ -24,12 +39,29 @@ createList = [Item "Apple", Item "Banana"]
 modifyList :: [ShoppingList String]
 modifyList = fmap (\(Item x) -> Item $x ++ "Functor") createList
 
-modifyListApp :: Maybe (ShoppingList String)
-modifyListApp =
-  Just (\(Item x) -> Item $ (x ++ "Applicative")) <*> Just (Item "Apple")
+modifyListApp :: ShoppingList String
+modifyListApp = Item (\x -> x ++ "Applicative") <*> Item "Apple"
 
-modifyListAppL :: [ShoppingList String]
-modifyListAppL =
-  [\(Item x) -> Item $ (x ++ "Applicative")] <*> [Item "Apple", Item "Banana"]
+modifyListDoubleApp :: [ShoppingList String]
+modifyListDoubleApp = [\x ->  Item (\y -> y ++ "Doubleapplicative")<*> x] <*> [Item "Apple", Item "Banana"]
+  --[(\x -> x)] <*> [Item "Apple", Item "Banana"]
 
-justify (Just x) = Just x
+
+modifyListMon :: ShoppingList String
+modifyListMon =  (Item "Apricot" >>= (\x -> Item x)) >>= (\x -> Item "Banana")
+
+
+logNumber :: Int -> Writer [String] Int
+logNumber x = writer (x, ["Got number: " ++ show x])
+
+multWithLog :: Writer [String] Int
+multWithLog = do
+    a <- logNumber 3
+    b <- logNumber 5
+    return (a*b)
+
+--multiWith :: Writer [String] Int
+--multiWith = 3 >>= (\x -> writer (x, ["Got number: " ++ show x])) 
+
+
+ 
