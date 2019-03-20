3ec74d0490036945ab0b3b9ca4441e3194dfb082
Tanmay <tanmay.bhattacharya2@conduent.com>
2019-03-20 13:16:42 +0530

    cm

--- src/Readfile.hs
+++ src/Readfile.hs
@@ -1,24 +1,49 @@
+{-# LANGUAGE ExistentialQuantification #-}
+
 module Readfile where
 
-import           System.Environment
 import           Control.Concurrent
-import           Control.Exception
+import           System.Environment
+
+--import           Control.Exception
+import           Control.Exception.Safe
+import           Data.Typeable
 import           System.Timeout
 
-catchAny :: IO a -> (SomeException -> IO a) -> IO a
-catchAny = Control.Exception.catch
+data CAClientExceptions
+  = InvalidInput String
+  | FileReadingException String
+  | OtherExceptions SomeException
+  deriving (Show, Typeable)
 
+instance Exception CAClientExceptions
+
+data SubExceptions =
+  NetworkFailure String
+  deriving (Show, Typeable)
+
+instance Exception SubExceptions where
+  toException = toException . OtherExceptions . SomeException
+  fromException se = do
+    OtherExceptions (SomeException e) <- fromException se
+    cast e
 
 readFromDisk :: FilePath -> IO String
-readFromDisk  f =  do
-    file <- readFile f
-    return file
+readFromDisk f = do
+  file <- readFile f
+  return file
 
-getContents :: IO ()
-getContents = do
+getContent :: IO ()
+getContent = do
   x <-
-    catchAny (readFromDisk "./asd.txt") $ \e -> do
-      putStrLn $ "Caught an exception: " ++ show e
-      return ""
-  putStrLn $ x
+    catch (readFromDisk "./asd.txt") $ \e -- why doesnt catchanywork
+     -> do
+      putStrLn $ "Caught an exception: "
+      Left e <- try $ throwIO $ FileReadingException "File not found"
+      return e
+  putStrLn $ x ----- Replace with bracket ?
 
+throwErrorSample :: IO ()
+throwErrorSample = do
+  e <- try $ throwIO $ NetworkFailure "Certificate not received"
+  print (e :: Either SomeException ())
